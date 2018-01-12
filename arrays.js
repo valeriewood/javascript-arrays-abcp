e77d2e2261a76ede7ddab2025b8582363b3b0270
Valerie Wood <valeriewd1@gmail.com>
2018-01-12 06:07:09 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -31,25 +31,61 @@ console.log(destructivelyAddElementToBeginningOfArray);
 
 
 
-var addElementToEndOfArray = ["Mashed Potatoes", "Green Beans"];
+const addElementToEndOfArray = ["Mashed Potatoes", "Green Beans"];
  
- addElementToBeginningOfArray.push("Corn");
+ addElementToEndOfArray.push("Corn");
 
 // we can assign it to the existing `cities` variable
 
-var destructivelyAddElementToEndOfArray = ["Silverware", ...addElementToBeginningOfArray];
+var destructivelyAddElementToEndOfArray = ["Silverware", ...addElementToEndOfArray];
 
-destructivelyAddElementToBeginningOfArray.push("Plate");
+destructivelyAddElementToEndOfArray.push("Plate");
 
-cities = ["Philadelphia", ...cities]
- 
-// but if we have a const
-const cats = ["Milo", "Garfield"]
- 
+
+ addElementToEndOfArray.push("Corn");
 // we need a new variable:
-const moreCats = ["Felix", ...cats]
- console.log(addElementToBeginningOfArray);
-console.log(destructivelyAddElementToBeginningOfArray);
+const destructivelyAddElementToEndOfArray = ["Felix", ...addElementToEndOfArray];
+
+ console.log(addElementToEndOfArray);
+console.log(destructivelyAddElementToEndOfArray);
+
+
+var accessElementInArray = ["Elizabeth Holmes", "Laurene Powell Jobs", "Arianna Huffington"];
+ 
+// the line below will print the string "Elizabeth Holmes"
+console.log(accessElementInArray[0]);
+ 
+// the code below will print the string "Arianna Huffington is the co-founder and editress-in-chief of The Huffington Post"
+var bio = " is the co-founder and editress-in-chief of The Huffington Post";
+console.log(accessElementInArray[2] + bio);
+ 
+// the line below will return undefined
+accessElementInArray[9];
 
 
 
+var destructivelyRemoveElementFromBeginningOfArray = ["Milo"];
+ 
+destructivelyRemoveElementFromBeginningOfArray.pop(0);
+ 
+console.log(destructivelyRemoveElementFromBeginningOfArray[0]);
+
+
+
+
+
+var removeElementFromBeginningOfArray = ["Milo"];
+ 
+removeElementFromBeginningOfArray.slice(1);
+ 
+var removeElementFromBeginningOfArray = ["Milo", "Garfield", "Otis"];
+ 
+removeElementFromBeginningOfArray = removeElementFromBeginningOfArray.slice(1);
+ removeElementFromBeginningOfArray;
+
+
+var destructivelyRemoveElementFromEndOfArray = ["chocolate", "vanilla", "raspberry"];
+ 
+destructivelyRemoveElementFromEndOfArray.slice(0, iceCreams.length - 1);
+ 
+destructivelyRemoveElementFromEndOfArray;
