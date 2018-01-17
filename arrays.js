6c7e3bca230026962b65cdc74a50b36e53ff1f6f
Valerie Wood <valeriewd1@gmail.com>
2018-01-17 04:12:56 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,10 +1,35 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 console.log(chocolateBars);
 
+
 var dinner = ["mashed potatoes", "green beans"];
 function addElementToBeginningOfArray(element, ...dinner) {
 return [element, ...dinner];
 }
 addElementToBeginningOfArray(dinner, "Sopranos"); 
 
+
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element);
+  return array;
+}
+
+
+
+
+function addElementToEndOfArray(array, element) {
+
+  newArr= [...array,element]
+
+  return newArr
+
+}
+
+
+
+
+function destructivelyAddElementToEndOfArray(array, element) { array.push(element)
+
+return array}
 
